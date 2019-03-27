6eaf66e4794dfadaf9cde6b0fa70e300d8e46464
poldrack <poldrack@gmail.com>
2019-03-26 19:26:59 -0700

    working version

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
-FROM bids/base_fsl
+FROM ubuntu
 
 # Install python and nibabel
 RUN apt-get update && \
     apt-get install -y python3 python3-pip && \
+    # pip3 install setuptools -U && \
+    # apt-get install  -y libblas-dev liblapack-dev libatlas-base-dev gfortran  && \
+    apt-get install -y nodejs npm && \
+    npm install bids-validator && \
+    pip3 install scipy && \
     pip3 install nibabel && \
-    apt-get remove -y python3-pip && \
+    pip3 install pybids && \
+    # apt-get remove -y python3-pip && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ENV PYTHONPATH=""
+ENV PATH /node_modules/bids-validator/bin:$PATH
 
 COPY run.py /run.py
 
--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+"""
+example of BIDS app for BIDS compmodels
+
+"""
+
 import argparse
 import os
 import subprocess
 import nibabel
 import numpy
 from glob import glob
+import pandas
+from bids import BIDSLayout, BIDSValidator
 
 __version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'version')).read()
@@ -27,20 +34,6 @@ def run(command, env={}):
 parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
 parser.add_argument('bids_dir', help='The directory with the input dataset '
                     'formatted according to the BIDS standard.')
-parser.add_argument('output_dir', help='The directory where the output files '
-                    'should be stored. If you are running group level analysis '
-                    'this folder should be prepopulated with the results of the'
-                    'participant level analysis.')
-parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
-                    'Multiple participant level analyses can be run independently '
-                    '(in parallel) using the same output_dir.',
-                    choices=['participant', 'group'])
-parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
-                   'corresponds to sub-<participant_label> from the BIDS spec '
-                   '(so it does not include "sub-"). If this parameter is not '
-                   'provided all subjects should be analyzed. Multiple '
-                   'participants can be specified with a space separated list.',
-                   nargs="+")
 parser.add_argument('--skip_bids_validator', help='Whether or not to perform BIDS dataset validation',
                    action='store_true')
 parser.add_argument('-v', '--version', action='version',
@@ -52,35 +45,30 @@ args = parser.parse_args()
 if not args.skip_bids_validator:
     run('bids-validator %s'%args.bids_dir)
 
-subjects_to_analyze = []
-# only for a subset of subjects
-if args.participant_label:
-    subjects_to_analyze = args.participant_label
-# for all subjects
-else:
-    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
-    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
 
-# running participant level
-if args.analysis_level == "participant":
+layout = BIDSLayout(args.bids_dir)
 
-    # find all T1s and skullstrip them
-    for subject_label in subjects_to_analyze:
-        for T1_file in glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
-                                         "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","anat", "*_T1w.nii*")):
-            out_file = os.path.split(T1_file)[-1].replace("_T1w.", "_brain.")
-            cmd = "bet %s %s"%(T1_file, os.path.join(args.output_dir, out_file))
-            print(cmd)
-            run(cmd)
+print(layout)
 
-# running group level
-elif args.analysis_level == "group":
-    brain_sizes = []
-    for subject_label in subjects_to_analyze:
-        for brain_file in glob(os.path.join(args.output_dir, "sub-%s*.nii*"%subject_label)):
-            data = nibabel.load(brain_file).get_data()
-            # calcualte average mask size in voxels
-            brain_sizes.append((data != 0).sum())
+# Ask get() to return the ids of subjects that have func files
+subcodes = layout.get(return_type='id', target='subject', suffix='events')
 
-    with open(os.path.join(args.output_dir, "avg_brain_size.txt"), 'w') as fp:
-        fp.write("Average brain size is %g voxels"%numpy.array(brain_sizes).mean())
+
+# compute mean RT for each run/subject
+subdata = []
+
+for s in subcodes:
+    print('processing subject',s)
+    func_files = layout.get(suffix='events', subject=s)
+    for f in func_files:
+        meanRT = pandas.read_csv(os.path.join(f.dirname,f.filename),sep='\t').query('isMissedTrial == 0').RT.mean()
+        subdata.append([s,f.entities['run'],meanRT])
+
+# create a data frame and save to derivatives
+subdata_df = pandas.DataFrame(subdata,columns=['subject','run','meanRT']).sort_values(['subject','run'])
+
+deriv_path = os.path.join(args.bids_dir,'derivatives/rt')
+if not os.path.exists(deriv_path):
+    os.makedirs(deriv_path)
+
+subdata_df.to_csv(os.path.join(deriv_path,'mean_rt.tsv'),sep='\t',index=False)
