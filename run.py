a3311cb664266bdd239631f23d8784942dea515c
poldrack <poldrack@gmail.com>
2019-03-27 10:27:33 -0700

    use derivatives dir by default

--- run.py
+++ run.py
@@ -37,14 +37,15 @@ def run(command, env={}):
 parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
 parser.add_argument('bids_dir', help='The directory with the input dataset '
                     'formatted according to the BIDS standard.')
-parser.add_argument('output_dir', help='The directory where the output files '
-                    'should be stored. If you are running group level analysis '
-                    'this folder should be prepopulated with the results of the'
-                    'participant level analysis.')
 parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
                     'Multiple participant level analyses can be run independently '
                     '(in parallel) using the same output_dir.',
                     choices=['participant', 'group'])
+parser.add_argument('output_dir', help='The directory where the output files '
+                    'should be stored. If you are running group level analysis '
+                    'this folder should be prepopulated with the results of the'
+                    'participant level analysis. Defaults to <bids dir>/derivatives/rt',
+                    nargs="?")
 parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
                    'corresponds to sub-<participant_label> from the BIDS spec '
                    '(so it does not include "sub-"). If this parameter is not '
@@ -64,16 +65,20 @@ args = parser.parse_args()
 if not args.skip_bids_validator:
     run('bids-validator %s'%args.bids_dir)
 
+if args.output_dir is not None:
+    output_dir = args.output_dir 
+else:
+    output_dir = os.path.join(args.bids_dir,'derivatives/rt')
 
-if not os.path.exists(args.output_dir):
-    os.makedirs(args.output_dir)
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 # load BIDS layout
 layout = BIDSLayout(args.bids_dir)
 print(layout)
 dataset_description = layout.description
 dataset_description['PipelineDescription']={'Name':'Mean RT analysis example'}
-with open(os.path.join(args.output_dir,'dataset_description.json'),'w') as f:
+with open(os.path.join(output_dir,'dataset_description.json'),'w') as f:
     json.dump(dataset_description,f)
 
 
@@ -103,7 +108,7 @@ if args.analysis_level == "participant":
             subdata.append([s,f.entities['run'],meanRT])
         subdata_df = pandas.DataFrame(subdata,columns=['subject','run','runMeanRT']).sort_values(['subject','run'])
 
-        sub_output_dir = os.path.join(args.output_dir,'sub-%s/func'%s)
+        sub_output_dir = os.path.join(output_dir,'sub-%s/func'%s)
         if not os.path.exists(sub_output_dir):
             os.makedirs(sub_output_dir)
 
@@ -125,4 +130,4 @@ elif args.analysis_level == "group":
         df = pandas.read_csv(f.path,sep='\t')
         groupdata.append([f.entities['subject'],df.runMeanRT.mean()])
     groupdata_df = pandas.DataFrame(groupdata,columns=['subject','meanRT']).sort_values(['subject'])
-    groupdata_df.to_csv(os.path.join(args.output_dir,'participants.tsv'),sep='\t',index=False)
+    groupdata_df.to_csv(os.path.join(output_dir,'participants.tsv'),sep='\t',index=False)
